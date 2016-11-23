3e7b85304e07593093f6b2020caae5297f76fa36
Remi Bergsma <github@remi.nl>
2016-11-23 23:03:11 +0100

    Add timeouts to the stop/start vm commands to prevent hangs

--- cloudstackops/cloudstackopsbase.py
+++ cloudstackops/cloudstackopsbase.py
@@ -46,6 +46,25 @@ except:
     sys.exit(1)
 
 
+class Timeout:
+    """Timeout class using ALARM signal."""
+    class Timeout(Exception):
+        pass
+
+    def __init__(self, sec):
+        self.sec = sec
+
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.raise_timeout)
+        signal.alarm(self.sec)
+
+    def __exit__(self, *args):
+        signal.alarm(0)    # disable alarm
+
+    def raise_timeout(self, *args):
+        raise Timeout.Timeout()
+
+
 class CloudStackOpsBase(object):
 
     # Init function
