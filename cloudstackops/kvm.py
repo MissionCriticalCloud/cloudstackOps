2aa92e899198dafa6b90d77fd94646046f7791d7
Boris Schrijver <bschrijver@schubergphilis.com>
2018-03-08 14:08:30 +0100

    fixup

--- cloudstackops/kvm.py
+++ cloudstackops/kvm.py
@@ -251,12 +251,14 @@ class Kvm(hypervisor.hypervisor):
         print "Note: Figuring out what OS Familiy the disk %s has"
 
         try:
-            with settings(host_string=self.ssh_user + "@" + kvmhost.ipaddress):
-                command = "cd %s; ls | grep \"\-sd\" | xargs -I {} qemu-img info {} | grep \"virtual size\" | awk '{ print substr($4,2) }'" % self.get_migration_path()
+            with settings(host_string=self.ssh_user + "@172.16.98.78"):
+                command = "cd %s; ls | grep \"\-sd\" | xargs -I {} qemu-img info {} | grep \"virtual size\" | awk '{ print substr($4,2) }'" % '/mnt/5b19833b-4412-30fb-9182-1a5a7ae3a882/migration/0014c4d1-462e-4965-93f2-0eeb6e97bac6'
+                print command
                 self.disks = fab.run(command)
                 print "Note: Disk sizes: %s" % self.disks.title()
                 return self.disks
-        except:
+        except Exception as e:
+            print e
             return False
 
     def modify_os_files(self, kvmhost, volume_uuid):
