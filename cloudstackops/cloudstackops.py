c4b130e07d1555ee7b3c087405bca8b89e55946c
Remi Bergsma <github@remi.nl>
2018-09-11 15:23:33 +0200

    Use findHostsForMigration to select host to migrate to

--- cloudstackops/cloudstackops.py
+++ cloudstackops/cloudstackops.py
@@ -19,6 +19,7 @@
 
 # Class to support tools used to operate CloudStack
 # Remi Bergsma - rbergsma@schubergphilis.com
+import json
 import logging
 import operator
 import re
@@ -1821,13 +1822,13 @@ class CloudStackOps(CloudStackOpsBase):
     # Migrate all vm's and empty hypervisor
     def emptyHypervisor(self, hostID, force_out_of_band_live_migration=False):
         # Host data
-        hostData = self.getHostData({'hostid': hostID})
-        foundHostData = hostData[0]
-        hostname = foundHostData.name
+        host_data = self.exoCsApi.listHosts(id=hostID)
+        current_host = host_data['host'][0]
+        hostname = current_host['name']
         to_slack = True
         if self.DEBUG == 1:
-            print hostData
-            print foundHostData
+            print host_data
+            print current_host
             print hostname
             to_slack = False
 
@@ -1862,39 +1863,55 @@ class CloudStackOps(CloudStackOpsBase):
                                 vmresult = 1
                             continue
 
-                        requested_memory = self.get_needed_memory(vm)
-                        migrationHost = self.findBestMigrationHost(
-                            foundHostData.clusterid,
-                            hostname,
-                            requested_memory)
-                        if not migrationHost:
+                        available_hosts = self.exoCsApi.findHostsForMigration(virtualmachineid=vm.id)
+                        available_hosts = sorted(available_hosts['host'], key=lambda k: k['memoryallocated'], reverse=False)
+
+                        for available_host in available_hosts:
+                            # Skip hosts that require storage migration
+                            if available_host['requiresStorageMotion']:
+                                print "Skipping %s because need storage_migration is %s" \
+                                      % (available_host['name'], available_host['requiresStorageMotion'])
+                                continue
+
+                            if available_host['clusterid'] != current_host['clusterid']:
+                                print "Skipping %s because part of another cluster" % available_host['name']
+                                continue
+
+                            if not available_host['suitableformigration']:
+                                continue
+
+                            print "Selecting %s" % available_host['name']
+                            break
+
+                        if not available_host:
                             print "\nError: No hosts with enough capacity to migrate vm's to. Please migrate manually to another cluster."
                             sys.exit(1)
 
                         # When OOB is forced
                         if force_out_of_band_live_migration:
-                            message = "Live migrating vm %s to host %s (OOB)" % (vm.name, migrationHost.name)
+                            message = "Live migrating vm %s to host %s (OOB)" % (vm.name, available_host['name'])
                             self.print_message(message=message, message_type="Note", to_slack=to_slack)
 
                             libvirtresult, libvirtoutput = self.ssh.migrateVirtualMachineViaLibVirt(
-                                {'hostname': hostname, 'desthostname': migrationHost.name, 'vmname': vm.instancename})
+                                {'hostname': hostname, 'desthostname': available_host['name'], 'vmname': vm.instancename})
                             if self.DEBUG == 1:
                                 print "Debug: Output: " + str(libvirtoutput) + " code " + str(libvirtresult)
 
-                            if libvirtresult == 0:
-                                return True
-                            return False
+                            if libvirtresult > 0:
+                                print "Debug: Migration of %s failed!" % vm.instancename
+
+                            continue
 
                         # When OOB is not forced
                         try:
-                            message = "Live migrating vm %s to host %s" % (vm.name, migrationHost.name)
+                            message = "Live migrating vm %s to host %s" % (vm.name, available_host['name'])
                             self.print_message(message=message, message_type="Note", to_slack=to_slack)
 
                             # Systemvm or instance
                             if bool(re.search('[rvs]-([\d])*-', vm.name)):
                                 vmresult = self.migrateSystemVm({
                                     'vmid': vm.id,
-                                    'hostid': migrationHost.id
+                                    'hostid': available_host['id']
                                 })
                                 instance = vm.name
                             else:
@@ -1902,7 +1919,7 @@ class CloudStackOps(CloudStackOpsBase):
                                     self.detach_iso(vm.id)
                                 vmresult = self.migrateVirtualMachine(
                                     vm.id,
-                                    migrationHost.id)
+                                    available_host['id'])
                                 instance = vm.instancename
                         except:
                             vmresult = 1
@@ -1916,14 +1933,14 @@ class CloudStackOps(CloudStackOpsBase):
                                     instance +
                                     "..), ")
                                 sys.stdout.flush()
-                                if foundHostData.hypervisor == "XenServer":
+                                if current_host.hypervisor == "XenServer":
                                     xapiresult, xapioutput = self.ssh.migrateVirtualMachineViaXapi(
-                                        {'hostname': hostname, 'desthostname': migrationHost.name, 'vmname': instance})
+                                        {'hostname': hostname, 'desthostname': available_host['name'], 'vmname': instance})
                                     if self.DEBUG == 1:
                                         print "Debug: Output: " + str(xapioutput) + " code " + str(xapiresult)
-                                if foundHostData.hypervisor == "KVM":
+                                if current_host.hypervisor == "KVM":
                                     libvirtresult, libvirtoutput = self.ssh.migrateVirtualMachineViaLibVirt(
-                                        {'hostname': hostname, 'desthostname': migrationHost.name, 'vmname': vm.instancename})
+                                        {'hostname': hostname, 'desthostname': available_host['name'], 'vmname': vm.instancename})
                                     if self.DEBUG == 1:
                                         print "Debug: Output: " + str(libvirtoutput) + " code " + str(libvirtresult)
 
